332a7a7cecf8da868f250453ba2edaa8bab742a5
Ahmed Ismail <your-email@github.com>
2025-11-25 23:08:54 +0200

    feat: Add a wide array of frontend assets including vendor libraries, icons, styles, and scripts.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,65 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::controller(DashboardController::class)->group(function () {
+
+    Route::any('/admin-login', 'admin_login')->name('admin.login');
+    Route::any('/admin-forget-password', 'admin_forget_password')->name('admin.forget.password');
+    Route::get('/admin-reset-password/{id}', 'admin_reset_password')->name('admin.reset.password');
+    Route::any('/admin-update-password', 'admin_update_password')->name('admin.forget.update');
+
+    Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
+
+        Route::get('/dashboard', 'dashboard')->name('dashboard');
+
+        Route::get('/admin/users', 'users_view')->name('users.view');
+        Route::get('/admin/users/block/{id}', 'users_block')->name('users.block');
+
+        Route::get('/admin/profile', 'admin_profile')->name('admin.profile');
+        Route::any('/admin/profile/update', 'admin_profile_update')->name('admin.profile.update');
+
+        Route::get('/admin/logout', 'admin_logout')->name('admin.logout');
+    });
+});
+
+
+
+Route::controller(BrandController::class)->group(function () {
+
+    Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
+
+        Route::get('/add-brand', 'add_brand')->name('add.brand');
+        Route::any('/store-brand', 'store_brand')->name('store.brand');
+        Route::get('/view-brand', 'view_brand')->name('view.brand');
+        Route::get('/edit-brand/{id}', 'edit_brand')->name('edit.brand');
+        Route::any('/update-brand', 'update_brand')->name('update.brand');
+        Route::get('/delete-brand/{id}', 'delete_brand')->name('delete.brand');
+    });
+});
+
+Route::controller(ProductController::class)->group(function () {
+
+    Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
+
+        Route::get('/add-product', 'add_product')->name('add.product');
+        Route::any('/store-product', 'store_product')->name('store.product');
+        Route::get('/view-products', 'view_products')->name('view.products');
+        Route::get('/edit-products/{id}', 'edit_product')->name('edit.product');
+        Route::any('/update-product', 'update_product')->name('update.product');
+        Route::get('/delete-product/{id}', 'delete_product')->name('delete.product');
+    });
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +67,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
